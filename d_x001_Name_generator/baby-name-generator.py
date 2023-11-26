56207851d1dd89b36f34613663d8c219807b5210
SarkerIT <sarker.data@gmail.com>
2023-11-26 01:16:16 -0600

    day2

--- d_x001_Name_generator/baby-name-generator.py
+++ d_x001_Name_generator/baby-name-generator.py
@@ -1,7 +1,25 @@
-# Baby name generator
-planet_name = input("What is the name of the planet you like most?")
-plant_name = input("What is the name of your favorite plant? (Plz insert one word)")
-surname = input("What is your surname?")
+# # Baby name generator
+# planet_name = input("What is the name of the planet you like most?")
+# plant_name = input("What is the name of your favorite plant? (Plz insert one word)")
+# surname = input("What is your surname?")
 
 print("\nSuggested baby name is: ")
-print(planet_name, plant_name, surname)
+# print(planet_name, plant_name, surname)
+
+
+# number of characters
+# print(len(input("What is your name? ")))
+
+# switch variables
+a = 5
+b = 7
+
+print(f"a= {a}, b={b}")
+b, a = a, b
+print(f"a= {a}, b={b}")
+
+
+# Name and no of charac with casting
+name = len(input("What is your name? "))
+print(name)
+print("Your name has " + str(name) + " characters")
