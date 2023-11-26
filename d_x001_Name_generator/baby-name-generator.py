df6ec849eb5b0fabcac58ed3ed16f2ff6e04d620
SarkerIT <sarker.data@gmail.com>
2023-11-26 00:07:30 -0600

    first commit day1

---
+++ d_x001_Name_generator/baby-name-generator.py
@@ -0,0 +1,7 @@
+# Baby name generator
+planet_name = input("What is the name of the planet you like most?")
+plant_name = input("What is the name of your favorite plant? (Plz insert one word)")
+surname = input("What is your surname?")
+
+print("\nSuggested baby name is: ")
+print(planet_name, plant_name, surname)
