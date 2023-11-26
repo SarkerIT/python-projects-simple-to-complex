6ddd5b18403c401a20c26065dd893177101e9bda
SarkerIT <sarker.data@gmail.com>
2023-11-26 10:00:38 -0600

    conditionals

---
+++ d_x001_Name_generator/leap_year.py
@@ -0,0 +1,26 @@
+year = int(input("Please enter year."))
+
+if year % 4 != 0:
+    print("Not leap year!")
+else:
+    if year % 100 != 0:
+        print("Leap year")
+    else:
+        if year % 400 == 0:
+            print("Leap year")
+        else:
+            print("NOT Leap year")
+
+
+# Alternate Versionn
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year")
+        else:
+            print("Not leap year")
+    else:
+        print("Leap year!")
+else:
+    print("Not leap year!")
