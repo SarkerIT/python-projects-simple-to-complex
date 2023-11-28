ef513276375cc3a19540f595cd345c2e67fcc151
SarkerIT <sarker.data@gmail.com>
2023-11-27 22:55:41 -0600

    professor class added

--- School_Management/main.py
+++ School_Management/main.py
@@ -27,12 +27,28 @@ class Student(Campus_people):
         print(f"Total points with extra credit: {credit_with_ex_credit}")
 
 
-# driver Code
-student_1 = Student("Rocky", "Pythony", 2020, "Computer engineering")
+class Professor(Campus_people):
+    def __init__(self, first, last, year_started, subject_teach, do_esearch) -> None:
+        super().__init__(first, last, year_started)
+        self.subject_teach = subject_teach
+        self.do_research = do_esearch
 
+
+## DRIVER Code
+# for student
+print("\n")
+student_1 = Student("Rocky", "Pythony", 2020, "Computer engineering")
 student_1.full_name()
 student_1.email()
-# student_1.major() # will not work; not a method but an attribute
+# student_1.major()  # will not work; not a method but an attribute
 # print(student_1.major()) # will not work not a method
 
 print(student_1.major)
+
+# For Professor
+print("\n")
+prof_1 = Professor("John", "Smith", 2020, "Phychology", "False")
+
+prof_1.email()
+print(prof_1.subject_teach)
+print(prof_1.do_research)
